0b31ff7d4b0f478e24e06d42b652ff49a11c1a8a
xie <709955748@qq.com>
2022-12-28 15:41:32 +0800

    build

---
+++ db-design/api.js
@@ -0,0 +1,77 @@
+import konva from "../libs/konva/konva.min.js"
+const { Stage, Layer, Rect, Group, Text } = konva
+
+//容器背景色
+const containerBackground = "#323437"
+
+//初始化容器
+export const intiContext = (container) => {
+    let stage = new Stage({ container, width: window.innerWidth, height: window.innerHeight })
+    let layer = new Layer()
+    stage.add(layer)
+    stage.getContainer().style.background = containerBackground
+    return { stage, layer }
+}
+
+//添加表格
+export const addTable = (layer, tableData) => {
+    const containerGroup = new Group({ draggable: true })
+    const locationConfig = { x: 20, y: 60, width: 300 }
+    //表格整个高度
+    let height = 0
+    //表格标题
+    let complexText = new Text({
+        ...locationConfig,
+        text: tableData.tableName,
+        fontSize: 14,
+        
+        fill: '#fff',
+        padding: 20,
+        align: 'center',
+    })
+    //标题背景
+    let rect = new Rect({
+        ...locationConfig,
+        fill: '#009933',
+        height: complexText.height(),
+        cornerRadius: [5, 5, 0, 0],
+    })
+    height += rect.height()
+    //所有的字段
+    const allFiledText = tableData.tableFields.map(item => {
+        let text = new Text({
+            ...locationConfig,
+            y: locationConfig.y + height,
+            text: item.name + ":" + item.type,
+            fontSize: 14,
+            fill: "#666",
+            padding: 15,
+            // align: 'center',
+        });
+        height += text.height()
+        return text;
+    })
+
+    //表格整体背景
+    let background = new Rect({
+        ...locationConfig,
+        fill: "#fff",
+        height: height,
+        cornerRadius: 5,
+        shadowColor: '#000',
+        shadowBlur: 10,
+        shadowOffsetX: 3,
+        shadowOffsetY: 3,
+        shadowOpacity: 0.2,
+        cornerRadius: 5,
+    });
+    containerGroup.add(background)
+    //表格内容
+    containerGroup.add(rect)
+    containerGroup.add(complexText)
+    allFiledText.forEach(item => containerGroup.add(item))
+
+    containerGroup.cache()
+    layer.add(containerGroup)
+    return containerGroup
+}
--- db-design/index.js
+++ db-design/index.js
@@ -1,31 +1,7 @@
-import "../libs/konva/konva.min.js"
+import { intiContext, addTable } from "./api.js"
 
-// https://konvajs.org/docs/overview.html
 
-let stage = new Konva.Stage({ container: 'container', width: window.innerWidth, height: window.innerHeight, });
+const { layer } = intiContext("container")
 
-// then create layer
-var layer = new Konva.Layer();
+addTable(layer,{tableName:"animal",comment:"动物",tableFields:[{name:"id",type:"int"},{name:"name",type:"string"}]})
 
-var pentagon = new Konva.RegularPolygon({
-  x: stage.width() / 2,
-  y: stage.height() / 2,
-  sides: 5,
-  radius: 70,
-  fill: 'red',
-  stroke: 'black',
-  strokeWidth: 4,
-  shadowOffsetX : 20,
-  shadowOffsetY : 25,
-  shadowBlur : 40,
-  opacity : 0.5
-});
-
-// add the shape to the layer
-layer.add(pentagon);
-
-// add the layer to the stage
-stage.add(layer);
-
-// draw the image
-layer.draw();
