bfb6bedb7584f072998e6331d1002eb26b4192da
xie <709955748@qq.com>
2023-01-04 17:45:04 +0800

    build

--- db-design/index.js
+++ db-design/index.js
@@ -3,5 +3,8 @@ import { intiContext, addTable } from "./api.js"
 
 const { layer } = intiContext("container")
 
-addTable(layer,{tableName:"animal",comment:"动物",tableFields:[{name:"id",type:"int"},{name:"name",type:"string"}]})
+addTable(layer,{tableName:"ct2_file",comment:"文件上传信息",tableFields:[{name:"id",type:"int"},{name:"title",type:"string"},{name:"path",type:"string"},{name:"type",type:"varchar"},{name:"size",type:"int"},{name:"update_date",type:"datetime"}]})
+
+
+addTable(layer,{tableName:"cu_animal",comment:"动物",tableFields:[{name:"id",type:"int"},{name:"natification",type:"string"}]})
 
